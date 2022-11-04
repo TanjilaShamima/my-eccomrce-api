d679bda6df5735eb910679164d4b8e2885bfda55
Tanjila Akter Shamima <tanjila.akter@bjitgroup.com>
2022-11-04 12:06:35 +0600

    update

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const PORT = process.env.PORT ||  5000;
+const UserRoute = require("./routes/user");
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
@@ -10,6 +11,12 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("successful"))
   .catch((e) => console.log(e));
+
+app.use("/api/user", UserRoute);
+
+app.get("/api/test", (req, res) => {
+  res.send("Helllllooo");
+});
 
 app.listen(PORT, () => {
   console.log("Backend server is running on ", PORT);
